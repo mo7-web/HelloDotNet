711b05afeb26951ee511607edd0af22f185afd6e
墨七 <mo7cc@outlook.com>
2026-08-24 12:34:49 +0800

    dotnet入门战役

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("The current time is " + DateTime.Now);
+
+string name = "墨七";
+
+Console.WriteLine("Hello, " + name + "! Welcome to the program.");
+
+var name2 = "墨七2";
+
+name2 = name2 + "3";
+
+Console.WriteLine("Hello2, " + name2 + "! Welcome to the program.");
--- NameSpace/Program.cs
+++ NameSpace/Program.cs
@@ -1,19 +1,4 @@
-﻿/*
-Console.WriteLine("The current time is " + DateTime.Now);
-
-string name = "墨七";
-
-Console.WriteLine("Hello, " + name + "! Welcome to the program.");
-
-var name2 = "墨七2";
-
-name2 = "3";
-
-Console.WriteLine("Hello2, " + name2 + "! Welcome to the program.");
-
- */
-
-namespace YourNamespace
+﻿namespace YourNamespace
 {
     class Program
     {
