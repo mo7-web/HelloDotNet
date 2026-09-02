4daaea5ac092b816de3985c4f0693b71f9191d37
墨七 <mo7cc@outlook.com>
2026-09-03 02:02:37 +0800

    dotnet学习

--- NameSpaceDemo/Program.cs
+++ NameSpaceDemo/Program.cs
@@ -1,38 +1,35 @@
-﻿namespace YourNamespace
+﻿class Program
 {
-    class Program
+    static async Task Main()
     {
-        static async Task Main()
+        static void MyFuncMo7()
         {
-            static void MyFuncMo7()
-            {
-                Console.WriteLine("Hello, MyFuncMo7");
-            }
-
-            Console.WriteLine("Hello, World!");
-
-            await Task.Delay(1000);
-
-            YourNamespace2.Program.MyFunc();
-
-            await Task.Delay(1000);
-
-            var ppppp = new YourNamespace3.Program();
-            ppppp.MyFunc();
-
-            await Task.Delay(1000);
-
-            YourNamespace4.Program.MyFunc();
-
-            await Task.Delay(1000);
-
-            MyFuncMo7();
-
-            await Task.Delay(1000);
-
-            Console.WriteLine("按任意键退出...");
-            Console.ReadKey();
+            Console.WriteLine("Hello, MyFuncMo7");
         }
+
+        Console.WriteLine("Hello, World!");
+
+        await Task.Delay(1000);
+
+        YourNamespace2.Program.MyFunc();
+
+        await Task.Delay(1000);
+
+        var ppppp = new YourNamespace3.Program();
+        ppppp.MyFunc();
+
+        await Task.Delay(1000);
+
+        YourNamespace4.Program.MyFunc();
+
+        await Task.Delay(1000);
+
+        MyFuncMo7();
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("按任意键退出...");
+        Console.ReadKey();
     }
 }
 
@@ -54,6 +51,17 @@ namespace YourNamespace3
         public void MyFunc()
         {
             Console.WriteLine("Hello, YourNamespace3");
+        }
+    }
+}
+
+namespace Foo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //...
         }
     }
 }
